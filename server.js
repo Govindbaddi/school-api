e0ade3cb8a56e1c5dc7eab0ab40edc8b0b6456c1
GovindBaddi <baddigovindarao710@gmail.com>
2026-03-06 21:44:18 +0530

    root route is added

--- server.js
+++ server.js
@@ -7,8 +7,15 @@ const router = require("./routes/schoolRouter");
 
 app.use(express.json())
 app.use("/api",router)
+
+
 app.use(bodyParser.json());
 const PORT=process.env.PORT || 5000;
+
+
+app.get("/", (req,res)=>{
+    res.send("School API is running successfully 🚀")
+})
 
 require("./config/db")
 app.listen(PORT,()=>{
